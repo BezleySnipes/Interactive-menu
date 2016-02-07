7dbe2ac9c50b7190d301165e9d09e3d7eb4fa662
BezleySnipes <David.Mulhall@gmail.com>
2016-02-07 18:12:24 +0000

    Initial commit

---
+++ Assets/colorChange.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class colorChange : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		public static Color black;
+	currentColor = color.black;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.R))
+		{
+            currentColor = color.blue;
+		}
+		
+		//color = gameObject.GetComponent<Renderer>().material.color ;
+	}
+}
